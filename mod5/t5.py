44c38b62a29297b70e807795c8ac74d243b29ce7
roopeia <roopeia@metropolia.fi>
2026-08-24 16:07:18 +0300

    mod5t5

--- mod5/t5.py
+++ mod5/t5.py
@@ -1,4 +1,4 @@
-#Kirjoita ohjelma, joka kysyy käyttäjältä käyttäjätunnuksen ja salasanan. 
+#  Kirjoita ohjelma, joka kysyy käyttäjältä käyttäjätunnuksen ja salasanan. 
 # Jos jompikumpi tai molemmat ovat väärin, tunnus ja salasana kysytään uudelleen. 
 # Tätä jatketaan kunnes kirjautumistiedot ovat oikein tai väärät tiedot on syötetty viisi kertaa. 
 # Edellisessä tapauksessa tulostetaan Tervetuloa ja jälkimmäisessä Pääsy evätty. 
